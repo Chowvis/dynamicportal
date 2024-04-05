6125570f03d6005626232aea67fd6fc444d7f1f3
Chowvis <142925746+Chowvis@users.noreply.github.com>
2024-04-05 12:41:29 +0530

    Initial commit

---
+++ resources/views/homepage/section3.blade.php
@@ -0,0 +1,5 @@
+<div class="w-full flex items-center relative">
+    <div class="font-bold text-xl z-10 p-3 opacity-50 absolute left-0 bg-white text-black flex justify-center items-center"><i class="fa-solid fa-arrow-left"></i></div>
+    <img src="logos/demo1.jpg" class="w-full h-auto" alt="">
+    <div class="font-bold text-xl z-10 p-3 opacity-50 absolute right-0 bg-white text-black flex justify-center items-center"><i class="fa-solid fa-arrow-right"></i></div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <title>Department of Education</title>
+</head>
+<body class="">
+    <section class="flex justify-center w-full">
+        <div class="max-w-[1280px] w-full flex justify-between items-center">
+            language
+        </div>
+    </section>
+
+
+    {{--header section 1--}}
+    <section class="flex justify-center w-full h-[120px] border border-x">
+        @include('homepage.section1')
+    </section>
+
+
+    {{--navbar section 2 --}}
+    <section class="flex justify-center w-full h-[44px] border-b border-collapse">
+      @include('homepage.section2')
+    </section>
+
+    {{-- banner: section 3 --}}
+    <section class="flex justify-center w-full">
+        @include('homepage.section3')
+    </section>
+
+</body>
+</html>
